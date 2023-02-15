32e77408af6cfaf7aa761a3c99027fbed19e49e2
sheila.orejuela <she.or.va@gmail.com>
2023-02-15 23:30:49 +0100

    Hago test Soldado

--- Actividad2_ED_Grupo_02/src/testJUnit5/TestSoldado.java
+++ Actividad2_ED_Grupo_02/src/testJUnit5/TestSoldado.java
@@ -20,17 +20,20 @@ import modelo.javabean.Soldado;
 class TestSoldado {
 	
 	//ATRIBUTOS 
+	private Soldado soldado;
 	private Soldado soldadoBueno;
 	private Soldado soldadoMalo;
 
 	@BeforeEach
 	void crearSoldados(){
-		soldadoBueno = new Soldado();
-		soldadoMalo = new Soldado();
+		soldado = new Soldado();
+		soldadoBueno = new Soldado(false, 10);
+		soldadoMalo = new Soldado(true, 10);
 	}
 	
 	@AfterEach
 	void borrarSoldados() {
+		soldado = null;
 		soldadoBueno = null;
 		soldadoMalo = null;
 	}
@@ -44,8 +47,8 @@ class TestSoldado {
 	 */
 	@Test
 	void testPuedeDisparar0() {
-		soldadoBueno.setNumeroBalas(0);
-		assertFalse(soldadoMalo.puedeDisparar());
+		soldado.setNumeroBalas(0);
+		assertFalse(soldado.puedeDisparar());
 	}
 	
 	
@@ -55,39 +58,41 @@ class TestSoldado {
 	 */
 	@Test
 	void testPuedeDisparar100() {
-		soldadoBueno.setNumeroBalas(100);
-		assertTrue(soldadoBueno.puedeDisparar());
+		soldado.setNumeroBalas(100);
+		assertTrue(soldado.puedeDisparar());
 	}
 	
-	
-	
+	//TEST METODO DISPARAR
+	/** PRUEBA 1
+	 * En este caso, el soldadoMalo está muerto y tiene 10 balas
+	 * El resultado esperado es True
+	 */
 	@Test
-	void testDispararno() {
-		soldadoBueno.setNumeroBalas(0);
-		if (soldadoBueno.getNumeroBalas() <=1) {
-		boolean resultadoEsperado = false;
-		boolean resultadoObtenido = soldadoBueno.isEstaMuerto();
-		assertEquals(resultadoEsperado , resultadoObtenido);
+	void testDispararSi() {
+		assertTrue(soldadoMalo.isEstaMuerto());
+		System.out.println(soldadoMalo.isEstaMuerto());
 		}
-	}
 	
+	/** PRUEBA 2
+	 *  En este caso, el soldadoBueno no está muerto, y tiene 10 balas
+	 *  El resultado esperado es False
+	 */
 	@Test
-	void testDispararsi() {
-		
-		soldadoBueno.setNumeroBalas(10);
-		soldadoMalo.setEstaMuerto(false);
-		
-		soldadoBueno.disparar(soldadoMalo);
-		int balasEsperadas = 9;
-		int balasObtenidas = soldadoBueno.getNumeroBalas();
-		assertEquals(balasEsperadas, balasObtenidas);
-		
-		boolean muerteEsperada = true;
-		boolean muerteObtenida = soldadoMalo.isEstaMuerto();
-		assertEquals(muerteEsperada, muerteObtenida);
-		
-		
-	}
+	void testDispararsNo() {
+		assertFalse(soldadoBueno.isEstaMuerto());
+		System.out.println(soldadoBueno.isEstaMuerto());
+		}
+	
+	/** PRUEBA 3
+	 * En este caso, el soldado sinBalas está muerto, y tiene 0 balas
+	 * El resultado esperado es True
+	 */
+	@Test
+	void testDispararSinBalas() {
+		Soldado sinBalas = new Soldado(true, 0);
+		assertTrue(sinBalas.isEstaMuerto());
+		}
+	
 	
 
 }
