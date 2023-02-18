09f7bbe32416402b376d97244dd1f8134489cdb4
ciberjbr <ciberjbr@gmail.com>
2023-02-18 02:32:04 +0100

    documentacion Soldado

--- Actividad2_ED/src/modelo/javabean/Soldado.java
+++ Actividad2_ED/src/modelo/javabean/Soldado.java
@@ -1,18 +1,53 @@
 package modelo.javabean;
 
 public class Soldado {
-	 
+	/**
+	 * atributo privado booleano que nos indica si nuestro soldado está vivo o muerto
+	 */
 	private boolean estaMuerto;
+	/**
+	 * atributo privado que nos indica el número de balas de nuestro soldado
+	 */
     private int numeroBalas;
-     //Crear los métodos “get” y “set” de los atributos cuando se vayan a hacer las pruebas y la documentación. Aquí no se han creado porque no aportan nada
+    
 
-    public boolean puedeDisparar() {
+    
+    public boolean isEstaMuerto() {
+		return estaMuerto;
+	}
+
+	public void setEstaMuerto(boolean estaMuerto) {
+		this.estaMuerto = estaMuerto;
+	}
+
+	public int getNumeroBalas() {
+		return numeroBalas;
+	}
+
+	public void setNumeroBalas(int numeroBalas) {
+		this.numeroBalas = numeroBalas;
+	}
+
+	/**
+	 * Este método nos indica si nuestro soldado puede disparar o no
+	 * @return devuelve un atributo booleano, true si puede disparar y 
+	 * false si no puede disparar 
+	 * si el número de balas es mayor que 0, devuelve true
+	 * si el número de balas es menor o igual que 0, devuelve false
+	 */
+	
+	public boolean puedeDisparar() {
     	if(this.numeroBalas > 0) {
     		return true;
     	}                          
     	return false;
     }
 
+	/**
+	 * Este método hace que nuestro soldado dispare a cambio de perder una bala 
+	 * y matar al Soldado que le pasamos por parámetro
+	 * @param sol hace referencia al soldado que va a morir  
+	 */
     public void disparar(Soldado sol) {
     	this.numeroBalas--;
     	sol.estaMuerto = true;
